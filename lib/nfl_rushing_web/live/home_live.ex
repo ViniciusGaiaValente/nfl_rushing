c2afcfbc8cad74dcc09a9a67085ef29de26657f7
ViniciusGaiaValente <viniciusvalente@pop-os.localdomain>
2021-10-28 22:34:56 -0300

    feat: responsive table to show results

--- lib/nfl_rushing_web/live/home_live.ex
+++ lib/nfl_rushing_web/live/home_live.ex
@@ -1,7 +1,11 @@
 defmodule NflRushingWeb.HomeLive do
   use NflRushingWeb, :live_view
 
+  alias NflRushing.Stats.Commands.IndexPlayerStats
+
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, game_previews: [])}
+    results = IndexPlayerStats.execute()
+
+    {:ok, assign(socket, %{name_filter: nil, sorting_options: [], results: results})}
   end
 end
